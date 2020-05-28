c60eb9c7e2fe5e059815b0fbdba93ae14078ca04
zh69ou <zhou69.1@qq.com>
2020-05-28 14:16:06 +0800

    添加样式

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 
 import { BsRouter } from "./config/bsrouter"
 // require('../assets/img/favicon.png')
+require('../assets/scss/bootstrap.scss')
 
 ReactDOM.render(
     <BsRouter />,
