2f91fd1814870f039a9216a3c136d5d97af80c61
zh69ou <zhou69.1@qq.com>
2020-06-22 17:56:58 +0800

    加载中调整

--- src/config/pub.tsx
+++ src/config/pub.tsx
@@ -1,11 +1,22 @@
-const userKey = 'user'
-const BaseUrl = '/api'
-
-const Api = {
+// 用户缓存名称
+export const userKey = 'user'
+// 接口地址
+export const BaseUrl = '/api'
+//接口地址
+export const Api = {
 	UserLogin:BaseUrl+'/login'
 }
-
-export{
-	userKey,
-	Api
+/**
+ * 加载中公用方法
+ * @Author zhou69.1@qq.com 2020-06-22
+ * @param  loading    加载显示状态
+ */
+export const IsLoad = (loading) => {
+	if(loading){
+		document.getElementById("loading").style.display = "flex"
+	}else{
+		setTimeout(()=>{
+    		document.getElementById("loading").style.display = "none"
+    	},500)
+	}
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,18 @@
 import React,{useState,useContext,useEffect} from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import {SetsContext} from "../state/sets"
-import {IsLoad} from "../components/pub"
+import {IsLoad} from "../config/pub"
 
 const DefIndex = (props) => {
 	let [loading,setLoad] = useState(true)
 	let psets = useContext(SetsContext)
 	useEffect(()=>{
-		setTimeout(()=>{
-			setLoad(false)
-		},1000)
+		IsLoad(false)
 	},[])
 	return (
-		<IsLoad loading={loading}>
-			<div className="bg-info">
-				<div>index:{psets.name}</div>
-			</div>
-		</IsLoad>
+		<div className="bg-info">
+			<div>index:{psets.name}</div>
+		</div>
 	)
 }
 export default DefIndex
