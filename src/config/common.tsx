b7b3ff863eee6923fceacb88bae7ff4708f1cd49
zh69ou <zhou69.1@qq.com>
2020-06-28 10:03:13 +0800

    字符串与对象切换方法

--- src/config/common.tsx
+++ src/config/common.tsx
@@ -1,24 +1,52 @@
 import qs from "qs"
 
 /**
+ * 对象转字符串
+ * @Author zhou69.1@qq.com 2020-06-28
+ */
+export const toStr = (obj)=>{
+	let st
+	try{
+		if(typeof(obj)!='string'){
+			st = qs.stringify(obj)
+		}else{
+			st = obj
+		}
+	}catch(e){
+		st = null
+	}
+	return st
+}
+
+/**
+ * 字符串转对象
+ * @Author zhou69.1@qq.com 2020-06-28
+ * @param  {[type]}        str        [description]
+ * @return {[type]}                   [description]
+ */
+export const toObj = (str)=>{
+	let sd
+	try{
+		var pattern = /^[a-z0-9]*=.*$/;
+        if(pattern.test(str)){
+        	sd = qs.parse(str)
+        }else{
+        	sd = str
+        }
+	}catch(e){
+		sd = null
+	}
+	return sd
+}
+
+/**
  * 获取缓存
  * @Author zhou69.1@qq.com 2020-06-24
  * @param   name       缓存名称
  */
 export const getData = (name)=>{
 	let data = localStorage.getItem(name)
-	let sd
-	try{
-		var pattern = /^[a-z0-9]*=.*$/;
-        if(pattern.test(data)){
-        	sd = qs.parse(data)
-        }else{
-        	sd = data
-        }
-	}catch(e){
-		sd = data
-	}
-	return sd
+	return toObj(data)
 }
 
 /**
@@ -28,12 +56,7 @@ export const getData = (name)=>{
  * @param       obj        缓存内容
  */
 export const setData = (name,obj)=>{
-	let st = ''
-	if(typeof(obj)!='string'){
-		st = qs.stringify(obj)
-	}else{
-		st = obj
-	}
+	let st = toStr(obj)
 	try{
 		localStorage.setItem(name, st)
 		return true
