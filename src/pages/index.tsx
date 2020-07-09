b3f05519e798511f4f2bc476ba197cdbb3f925ca
zh69ou <zhou69.1@qq.com>
2020-07-09 15:21:55 +0800

    删除测试内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,13 @@
 import React,{useState,useContext,useEffect} from "react"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import {SetsContext} from "../state/sets"
 import {IsLoad} from "../config/pub"
-import AlertMsg from "../components/alert"
-
-import "../components/scss/alert.scss"
 
 const DefIndex = (props) => {
-	let psets = useContext(SetsContext)
-	let [salert,upSalert] = useState(false)
-	let [time,upTime] = useState(0)
+
 	useEffect(()=>{
 		IsLoad(false)
 	},[])
 	return (
 		<div className="bg-info">
-			<span onClick={()=>{
-				AlertMsg({children:'<a>abc</a>',auclose:true}).then((res)=>{
-					console.log(res)
-				})
-			}}>显示</span>
 		</div>
 	)
 }
