5ef0d8392a171f725ffbce8dbd7f44617b6acac8
zh69ou <zhou69.1@qq.com>
2020-07-22 14:22:49 +0800

    面包屑示例

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,20 @@
 import React,{useState,useContext,useEffect} from "react"
 import {IsLoad} from "../config/pub"
 
-import {ListBox} from "../components/list"
-import "../components/scss/list.scss"
+import {Crumb} from "../components/crumb"
+import "../components/scss/crumb.scss"
 
 const DefIndex = (props) => {
 	useEffect(()=>{
 		IsLoad(false)
 	},[])
-	const editact = (obj)=>{
-		console.log(obj)
-	}
-	let editbut = (
-			<button>编辑</button>
-		)
-	let delbut = (
-			<button>删除</button>
-		)
-	let hlist = ['编号','名称','添加时间','操作']
-	let data = [
-		{id:1,name:'测试1',desc:'测试描述',content:'测试内容',addtime:1595217198},
-		{id:2,name:'测试2',desc:'测试描述',content:'测试内容',addtime:1595217198},
-		{id:3,name:'测试3',desc:'测试描述',content:'测试内容',addtime:1595217198},
-	]
-	let field = [
-		{type:'txt',code:'id',},
-		{type:'txt',code:'name',},
-		{type:'date',code:'addtime',},
-		{type:'but',code:[{html:editbut,k:'',v:'',callback:(info)=>{editact(info)}},{html:delbut,k:'id',v:'1'}]},
+	let menu = [
+		{id:1,name:'测试1',url:'/'},
+		{id:2,name:'测试2',url:'/'},
+		{id:3,name:'测试3',url:'/'},
 	]
 	return (
-		<ListBox hlist={hlist} data={data} field={field}></ListBox>
+		<Crumb menu={menu}><a>添加</a></Crumb>
 	)
 }
 export default DefIndex
