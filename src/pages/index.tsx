a620a49c41f226274e4977fc04261d0a8afe45bd
zh69ou <zhou69.1@qq.com>
2020-07-27 09:55:37 +0800

    滚动到底部测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,26 @@ import {IsLoad} from "../config/pub"
 import {RollIs} from "../components/roll"
 
 const DefIndex = (props) => {
+	const [num,setNum] = useState(30)
+	const hlist = ()=>{
+		let ht = []
+		for (var i = 0; i < num; i++) {
+			ht.push(<div key={i}><a>添加-{i}</a></div>)
+		}
+		return ht
+	}
 	useEffect(()=>{
 		IsLoad(false)
 	},[])
 	RollIs(props).then((res)=>{
-		console.log(res)
+		if(num<100){
+			setNum(num+10)
+		}
+		console.log(num)
 	})
 	return (
 		<div>
-		<div><a>添加</a></div>
+			{hlist()}
 		</div>
 	)
 }
