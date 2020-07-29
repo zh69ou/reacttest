13c9e7145cc34f0af358a20c9ef93c86a48f14d6
zh69ou <zhou69.1@qq.com>
2020-07-29 10:50:29 +0800

    下拉测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
 import React,{useState,useContext,useEffect} from "react"
 import {IsLoad} from "../config/pub"
 
-import {DateBox} from "../components/date"
-import "../components/scss/date.scss"
+import {DropDown} from "../components/dropdown"
+import "../components/scss/dropdown.scss"
 
 const DefIndex = (props) => {
 	useEffect(()=>{
 		IsLoad(false)
 	},[])
 	return (
-		<div>
-			<DateBox choice="1" callback={(res)=>{}}></DateBox>
+		<div className="d-flex justify-content-around">
+			<DropDown>
+				<div className="dropdown-item active">Cras justo odio</div>
+				<div className="dropdown-item">Dapibus ac facilisis in</div>
+				<div className="dropdown-item">Morbi leo risus</div>
+				<div className="dropdown-item">Porta ac consectetur ac</div>
+				<div className="dropdown-item">Vestibulum at eros</div>
+			</DropDown>
+			<DropDown>
+				<div className="dropdown-item active">Cras justo odio</div>
+				<div className="dropdown-item">Dapibus ac facilisis in</div>
+				<div className="dropdown-item">Morbi leo risus</div>
+				<div className="dropdown-item">Porta ac consectetur ac</div>
+				<div className="dropdown-item">Vestibulum at eros</div>
+			</DropDown>
+			<DropDown>
+				<div className="dropdown-item active">Cras justo odio</div>
+				<div className="dropdown-item">Dapibus ac facilisis in</div>
+				<div className="dropdown-item">Morbi leo risus</div>
+				<div className="dropdown-item">Porta ac consectetur ac</div>
+				<div className="dropdown-item">Vestibulum at eros</div>
+			</DropDown>
 		</div>
 	)
 }
